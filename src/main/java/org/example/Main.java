2a8aa58b177298ffdc9d118d7e25da154e10ffb6
Max <moimail312@gmail.com>
2024-07-01 00:35:38 +0600

    add text1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         Service firstService = new Service("First service");
-        firstService.action();
+
     }
 }
