d35d72a96a04aaff1c3bd6a4aa7bba9d50578bcd
Max <moimail312@gmail.com>
2024-07-01 01:07:57 +0600

    Start version

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,8 @@
 package org.example;
-//new comment2
+
 public class Main {
     public static void main(String[] args) {
         Service firstService = new Service("First service");
-
+        firstService.action();
     }
 }
