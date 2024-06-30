8508a0ea214ab582b840b82a9f0c12982319480c
Max <moimail312@gmail.com>
2024-06-30 23:34:11 +0600

    Add action in main.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,9 +4,7 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
         Service firstService = new Service("First service");
-
-
+        firstService.action();
     }
 }
