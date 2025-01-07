e66b57df1e13e3d3ebed8da1ce753f3bcb25978b
Max <moimail312@gmail.com>
2025-01-07 16:48:31 +0600

    forth branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,10 +4,5 @@ public class Main {
     public static void main(String[] args) {
         Service firstService = new Service("First service");
         firstService.action();
-        firstService.think();
-        firstService.sleep();
-        firstService.run();
-        firstService.eat();
-        firstService.love();
     }
 }
